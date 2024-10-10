e2f1aa6021f450897c7528f08b509ac82219a2ad
sergio pérez <sergio@hackstler.com>
2024-10-10 11:25:18 +0200

    fix typo

--- 01-FRONTEND/Teoria/01-JavaScript/08-Bucles/matriz.js
+++ 01-FRONTEND/Teoria/01-JavaScript/08-Bucles/matriz.js
@@ -1,14 +1,14 @@
-const matrizCalificaiones = [
+const matrizCalificaciones = [
     [3,5,9], // Calificaciones estudiantes número 1
     [2,3,4], // Calificaciones estudiantes número 2
     [9,8,9] // Calificaciones estudiantes número 3
 ]
 
 
-for (let i = 0; i < matrizCalificaiones.length; i++){
+for (let i = 0; i < matrizCalificaciones.length; i++){
 
-    for(let j = 0; j < matrizCalificaiones[i].length; j++){
-        console.log(`Estudiante ${i+1} con la asignatura ${j+1} y la nota ${matrizCalificaiones[i][j]}`) // matriz/primerArray/primerElementoDelArray
+    for(let j = 0; j < matrizCalificaciones[i].length; j++){
+        console.log(`Estudiante ${i+1} con la asignatura ${j+1} y la nota ${matrizCalificaciones[i][j]}`) // matriz/primerArray/primerElementoDelArray
     }
 
 

ff995cbb01fa9828e3c5a1a1ebdf2a0a921e86cd
sergio pérez <sergio@hackstler.com>
2024-10-14 19:37:25 +0200

    create calculadora

---
+++ 01-FRONTEND/Teoria/01-JavaScript/07-Funciones/repaso/calcualdora.js
@@ -0,0 +1,16 @@
+// Desarrollador una calculadora super simple
+
+function multiplicar(num1, num2) {
+    return num1 * num2
+}
+
+
+// Añadir metodos de operaciones a el objeto calculadora
+
+const calculadora = {
+    sumar: "sumar", restar: "restar", dividir:"dividir", multiplicar
+}
+
+
+
+

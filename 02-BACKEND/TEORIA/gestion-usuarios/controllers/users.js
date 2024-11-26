21c09906e19a0f029d5ec257bf8eb20369b98b08
sergio pérez <sergio@hackstler.com>
2024-11-26 21:46:03 +0100

    changes

--- 02-BACKEND/TEORIA/gestion-usuarios/controllers/users.js
+++ 02-BACKEND/TEORIA/gestion-usuarios/controllers/users.js
@@ -31,4 +31,51 @@ const listarUsuario =(req, res)=>{
     res.send('Usuario no encontrado')
 }
 
-module.exports = {leerUsarios, listarUsarios, listarUsuario}
+const guardarUsuarios=(usuarios)=>{
+    const data = fs.writeFileSync(usuariosPath, JSON.stringify(usuarios))
+    console.log("🚀 ~ escribirUsuario ~ data:", data)
+}
+
+const crearUsuario =(req, res)=>{
+    const usuarios = leerUsarios() // [{}]
+    const newUser = req.body  //{    "nombre": "Carlos Gómez", "email": "carlos.gomez@example.com"}
+    newUser.id = usuarios.length + 1
+    newUser //{ id: 4,   "nombre": "Carlos Gómez", "email": "carlos.gomez@example.com"}
+    usuarios.push(newUser)
+    guardarUsuarios(usuarios)
+    res.status(200).json(newUser)
+
+}
+
+const actualizarUsuario =(req, res)=>{
+    const usuarios = leerUsarios()
+    const newInfoUser = req.body
+    const id = +req.params.id
+    const index = usuarios.findIndex(user=> user.id === id)
+
+    if(index === -1){ // que el id no existe en nuestros usuarios
+        return res.status(404).json({error: 'Usuario no encontrado'})
+    }
+
+    // { "nombre": "Sergio Muñoz", "email": "sergio.muñoz@example.com", "id": 5 } usuarios[index]
+    // { "nombre": "Sergio Perez", "email": "sergio.muñoz@example.com", "address: "calle valmojado 152"} newInfoUser
+    usuarios[index] = {...usuarios[index], ...newInfoUser}  // { "nombre": "Sergio Perez", "email": "sergio.muñoz@example.com", id: 5,  "address: "calle valmojado 152"}
+    guardarUsuarios(usuarios)
+
+    res.status(200).json(usuarios[index])
+
+}
+const eliminarUsuario =(req, res)=>{
+    const usuarios = leerUsarios()
+    const id =  +req.params.id
+    const usersFiltered = usuarios.filter(user=>user.id !== id)
+
+    if(usersFiltered.length === usuarios.length){ // que el id no existe en nuestros usuarios
+        return res.status(404).json({error: 'Usuario no encontrado'})
+    }
+    guardarUsuarios(usersFiltered)
+    res.status(200).send()
+
+
+}
+module.exports = {leerUsarios, listarUsarios, listarUsuario, crearUsuario, actualizarUsuario, eliminarUsuario}
--- 02-BACKEND/TEORIA/gestion-usuarios/routes/users.js
+++ 02-BACKEND/TEORIA/gestion-usuarios/routes/users.js
@@ -2,14 +2,15 @@ const express = require('express')
 
 const router = express.Router()
 
-const { listarUsarios , listarUsuario} = require('../controllers/users')
+const { listarUsarios , listarUsuario, crearUsuario, actualizarUsuario, eliminarUsuario} = require('../controllers/users')
 
 
 
 router.get('/',listarUsarios ) // endpoint para devolver todos usuarios
 router.get('/:id', listarUsuario) // endpoint para devolver un usuario por id
-router.post('/')
-router.put('/:id')
+router.post('/', crearUsuario)
+router.put('/:id', actualizarUsuario)
+router.delete('/:id', eliminarUsuario)
 
 
 module.exports = router

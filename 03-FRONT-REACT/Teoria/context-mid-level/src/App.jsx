36ef0a6bf52d03fd63db65d092703b7b0f6a1ab0
sergio pérez <sergio@hackstler.com>
2025-01-22 20:57:23 +0100

    add context examples 2

--- 03-FRONT-REACT/Teoria/context-mid-level/src/App.jsx
+++ 03-FRONT-REACT/Teoria/context-mid-level/src/App.jsx
@@ -2,13 +2,41 @@ import { useState, createContext, useContext } from 'react'
 
 import './App.css'
 
+const ThemeContext = createContext()
+
 function App() {
+  const [theme, setTheme] = useState('dark')
+
+  const toggleTheme =()=>{
+    setTheme((prev)=> prev === 'dark' ? 'light' : 'dark')
+  }
 
   return (
     <>
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+      <Header/>
+    </ThemeContext.Provider>
 
     </>
   )
+}
+
+
+function Header() {
+  return <Navbar />;
+}
+
+function Navbar() {
+  return <Button />;
+}
+
+function Button() {
+
+  const {theme, toggleTheme} = useContext(ThemeContext)
+  console.log("🚀 ~ Button ~ theme:", theme)
+
+  return <button style={{background: `${theme === 'dark' ? '#242424': '#FFFFFF'}`}} onClick={toggleTheme}>{theme === 'dark' ? 'CAMBIAR A MODO NORMAL': ' CAMBIAR A MODO OSCURO'}</button>
+  
 }
 
 export default App
